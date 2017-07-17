6997d20d0e9221ee628f257ecf53dbe23811021b
Harshith <harshith100gp@gmail.com>
2017-07-17 10:52:43 -0700

    Update mergeSort.java

--- mergeSort.java
+++ mergeSort.java
@@ -22,63 +22,63 @@ class mergeSort {
 
 		for (i = 0; i < n1; i++)
         	L[i] = a[l + i];
-    	for (j = 0; j < n2; j++)
+    		for (j = 0; j < n2; j++)
         	R[j] = a[m + 1+ j];
 
-        i=0;j=0;
+        	i=0;j=0;
 		int k=l;
 		while (i < n1 && j < n2)
-    	{
-           if (L[i] <= R[j])
-        	{
-           		 a[k] = L[i];
-            	i++;
-        	}
-        	else
-        	{
-            	a[k] = R[j];
-            	j++;
-        	}
-        	k++;
-    	}
+    		{
+          		 if (L[i] <= R[j])
+        		{
+           			 a[k] = L[i];
+            			 i++;
+        		}
+        		else
+        		{
+            			a[k] = R[j];
+            			j++;
+        		}
+        		k++;
+    		}
  
-    /* Copy the remaining elements of L[], if there
-       are any */
-    	while (i < n1)
-    	{
-        	a[k] = L[i];
-        	i++;
-        	k++;
-    	}
+	    /* Copy the remaining elements of L[], if there
+	       are any */
+		while (i < n1)
+		{
+			a[k] = L[i];
+			i++;
+			k++;
+		}
  
-    /* Copy the remaining elements of R[], if there
-       are any */
-    	while (j < n2)
-    	{
-       		a[k] = R[j];
-        	j++;
-        	k++;
-    	}
-
-    	System.out.println("After MergeSort");
-		for(int z:a)
-			System.out.print(z+" ");
-
-		System.out.println(""); 
-		
-	}
-
-	public static void main(String args[]){
-
-		
-		int a[]={3,2,1,-1,0,-13,1,1,4,6,4,2,18,3,0};
-		merge_sort(a,0,a.length-1);
+	    /* Copy the remaining elements of R[], if there
+	       are any */
+		while (j < n2)
+		{
+			a[k] = R[j];
+			j++;
+			k++;
+		}
 
 		System.out.println("After MergeSort");
-		for(int z:a)
-			System.out.print(z+" ");
+			for(int z:a)
+				System.out.print(z+" ");
 
-		System.out.println(""); 
+			System.out.println(""); 
 
-	}
+		}
+
+		public static void main(String args[]){
+
+
+			int a[]={3,2,1,-1,0,-13,1,1,4,6,4,2,18,3,0};
+			merge_sort(a,0,a.length-1);
+
+			System.out.println("After MergeSort");
+			for(int z:a)
+				System.out.print(z+" ");
+
+			System.out.println(""); 
+
+		}
 }
